c07a362500eb5464a293ca9cd2b447375ddc77ff
unknown <swapnils@atjoin.in>
2023-12-11 17:20:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let boxes=document.querySelectorAll(".box");
 
-let resetBtn=document.querySelector("#reset");
+let resetBtn=document.querySelector(".reset");
 
-let newbtn=document.querySelector("#new-btn");
+let newbtn=document.querySelector("#newbtn");
 
-let message=document.querySelector(".msg-container");
+let msgContainer=document.querySelector(".msg-container");
 
 let msg=document.querySelector("#msg");
 let turnO=true;
@@ -12,6 +12,11 @@ let turnO=true;
 const winPatterns=[[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[3,4,6],[3,4,5],[6,7,8]]
 
 
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide")
+}
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         console.log("edcfvc");
@@ -29,6 +34,24 @@ boxes.forEach((box)=>{
     })
 })
 
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congrulations,WInner is ${winner}`
+    msgContainer.classList.remove("hide")
+    disableBoxes();
+}
 const checkWinner=()=>{
     for (let pattern of winPatterns){
         let pos1Val=    boxes[pattern[0]].innerText;
@@ -39,7 +62,11 @@ const checkWinner=()=>{
         {
             if(pos1Val==pos2Val && pos2Val==pos3Val){
                 console.log("Winner");
+                showWinner(pos1Val)
             }
         }
     }
 }
+
+newbtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
